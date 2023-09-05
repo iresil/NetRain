78f3b2f8882d2c8d58cdb9665ef2bc17fa223530
Irene Spanou <e.spanou.82@gmail.com>
2023-09-05 23:55:14 +0300

    Added .svg files to project as resources

--- Resource.h
+++ Resource.h
@@ -1,3 +1,70 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
 // Used by app.rc
+//
+#define IDR_VECTOR1                     101
+#define IDR_VECTOR2                     102
+#define IDR_VECTOR3                     103
+#define IDR_VECTOR4                     104
+#define IDR_VECTOR5                     105
+#define IDR_VECTOR6                     106
+#define IDR_VECTOR7                     107
+#define IDR_VECTOR8                     108
+#define IDR_VECTOR9                     109
+#define IDR_VECTOR10                    110
+#define IDR_VECTOR11                    111
+#define IDR_VECTOR12                    112
+#define IDR_VECTOR13                    113
+#define IDR_VECTOR14                    114
+#define IDR_VECTOR15                    115
+#define IDR_VECTOR16                    116
+#define IDR_VECTOR17                    117
+#define IDR_VECTOR18                    118
+#define IDR_VECTOR19                    119
+#define IDR_VECTOR20                    120
+#define IDR_VECTOR21                    121
+#define IDR_VECTOR22                    122
+#define IDR_VECTOR23                    123
+#define IDR_VECTOR24                    124
+#define IDR_VECTOR25                    125
+#define IDR_VECTOR26                    126
+#define IDR_VECTOR27                    127
+#define IDR_VECTOR28                    128
+#define IDR_VECTOR29                    129
+#define IDR_VECTOR30                    130
+#define IDR_VECTOR31                    131
+#define IDR_VECTOR32                    132
+#define IDR_VECTOR33                    133
+#define IDR_VECTOR34                    134
+#define IDR_VECTOR35                    135
+#define IDR_VECTOR36                    136
+#define IDR_VECTOR37                    137
+#define IDR_VECTOR38                    138
+#define IDR_VECTOR39                    139
+#define IDR_VECTOR40                    140
+#define IDR_VECTOR41                    141
+#define IDR_VECTOR42                    142
+#define IDR_VECTOR43                    143
+#define IDR_VECTOR44                    144
+#define IDR_VECTOR45                    145
+#define IDR_VECTOR46                    146
+#define IDR_VECTOR47                    147
+#define IDR_VECTOR48                    148
+#define IDR_VECTOR49                    149
+#define IDR_VECTOR50                    150
+#define IDR_VECTOR51                    151
+#define IDR_VECTOR52                    152
+#define IDR_VECTOR53                    153
+#define IDR_VECTOR54                    154
+#define IDR_VECTOR55                    155
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        156
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif

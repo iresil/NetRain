dbd06aa7045d7882d64818440a90182644000a65
Jason P <586496+Vartalus@users.noreply.github.com>
2023-09-23 17:39:15 +0300

    Added handling for c & p screensaver args

--- NetRain_UI/NetRain.cpp
+++ NetRain_UI/NetRain.cpp
@@ -5,8 +5,20 @@ using namespace System;
 using namespace System::Windows::Forms;
 
 [STAThread]
-int main()
+int main(array<String^>^ args)
 {
+    for (int i = 0; i < args->Length; i++)
+    {
+        String^ cleanArg = args[i]->Trim('\\')->Trim('=')->Trim('/');
+        if (cleanArg == "c")
+        {
+            System::Windows::Forms::MessageBox::Show("No Settings Available");
+            return 0;
+        }
+        else if (cleanArg == "p")
+            return 0;
+    }
+
     Application::EnableVisualStyles();
     Application::SetCompatibleTextRenderingDefault(false);
     Application::Run(gcnew NetRain::MatrixForm());
